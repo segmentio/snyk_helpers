558998ace790498eadee88a2d4615f733b2f4a6d
Leif <leif@segment.com>
2018-06-22 10:25:54 -0700

    Update snyk.sh

--- initialization/snyk.sh
+++ initialization/snyk.sh
@@ -2,9 +2,9 @@ LATEST_RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/sny
 LATEST_VERSION=$(echo $LATEST_RELEASE | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
 BINARY_URL="https://github.com/snyk/snyk/releases/download/$LATEST_VERSION/snyk-linux"
 
-"${SNYK_LEVEL:?Need to set SNYK_LEVEL = 'RPRT' in the CircleCI environment variables settings.}"
-"${SNYK_ORG:?Need to set SNYK_ORG = 'segment_pro' in the CircleCI environment variables settings.}"
-"${SNYK_TOKEN:?Need to set 'context: snyk' in your .circleci/config.yml workflow.}"
+[ -z "$SNYK_LEVEL" ] && { echo "Set SNYK_LEVEL = 'RPRT' in the CircleCI environment variables settings"; exit 1; }
+[ -z "$SNYK_ORG" ] && { echo "Set SNYK_ORG = 'segment_pro' in the CircleCI environment variables settings"; exit 1; }
+[ -z "$SNYK_TOKEN" ] && { echo "Set 'context: snyk' in your .circleci/config.yml workflow"; exit 1; }
 
 curl -sL "$BINARY_URL" -o snyk-linux
 chmod +x snyk-linux
