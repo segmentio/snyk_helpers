a8ded4ea205432e5f7198009407587b8904a4ed7
Evan Culver <evan.culver@segment.com>
2020-03-16 20:11:31 -0700

    s/macos/linux

--- initialization/snyk.sh
+++ initialization/snyk.sh
@@ -4,7 +4,7 @@
 
 LATEST_RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/snyk/snyk/releases/latest)
 LATEST_VERSION=$(echo $LATEST_RELEASE | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
-BINARY_URL="https://github.com/snyk/snyk/releases/download/$LATEST_VERSION/snyk-macos"
+BINARY_URL="https://github.com/snyk/snyk/releases/download/$LATEST_VERSION/snyk-linux"
 NEVER_FAIL=false
 
 curl -sL "$BINARY_URL" -o snyk
